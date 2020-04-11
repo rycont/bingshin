0cfd127ce6955ca6797e12873ef4ed26189f3caa
reactericdev <reactdev@kakao.com>
2020-04-11 22:48:45 +0900

    Regular updates

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 chrome.storage.sync.get('bingshin', ({ bingshin: enabled }) => {
-  if (enabled === false) return
+  if (!enabled) return
+  document.body.style.display = 'none'
   chrome.storage.sync.set({ bingshin: true })
 
   const address = location.href
-  if (address.includes('search?q')) location.href = location.href.replace('bing', 'google')
+  if (address.includes('search?q'))
+    location.href = location.href.replace('bing', 'google')
   else location.href = 'https://google.com'
 })
